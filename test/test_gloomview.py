0dfc4ae886ea4a04a3e0f53ee76d787bb52582c5
Carlo van Overbeek <carlovo@gmail.com>
2020-02-07 08:14:42 +0100

    refactor question unit testing

--- test/test_gloomview.py
+++ test/test_gloomview.py
@@ -4,17 +4,24 @@ import unittest
 from unittest.mock import patch
 import os
 import sys
-sys.path.insert(0, '../')
-sys.path.insert(0, './')
+sys.path.insert(0, "../")
+sys.path.insert(0, "./")
 from gloomview import UserInterface, UserInterfaceMain, UserInterfaceSave  # noqa
 
 
-logging.basicConfig(level=logging.WARN, format='')
+logging.basicConfig(level=logging.WARN, format="")
 
 
-class TestMultipleChoiceQuestion(unittest.TestCase):
+class UserInterfaceCopy(UserInterface):
     """
-    Test Gloomuser's multipleChoiceQuestion function
+    UserInterface is an abstract class, but its functionality should still be tested
+    """
+    pass
+
+
+class TestUserInterfaceCopy(unittest.TestCase):
+    """
+    Test Gloomview's UserInterface class
     """
 
     @classmethod
@@ -24,185 +31,218 @@ class TestMultipleChoiceQuestion(unittest.TestCase):
         """
 
         logging.info(
-            "Setting up variables for testing multipleChoiceQuestion function")
+            "Setting up variables for testing Gloomview's UserInterface class"
+        )
 
-        cls.faulOptions = ('okay', 'okay')
+        cls.user_interface = UserInterfaceCopy()
 
-        cls.questionStr = "Choose a taste."
-        cls.answersStr = ('sweet', 'sour', 'bitter')
+        cls.faul_options = ("okay", "okay")
+
+        cls.question_str = "Choose a taste."
+        cls.answers_str = ("sweet", "sour", "bitter")
 
         # technically all user input is str, but it seems good to check correct parsing of ints
-        cls.questionInt = "What is your lucky number on a 6-sided die?"
+        cls.question_int = "What is your lucky number on a 6-sided die?"
         # remember: ranges include only starting number!
-        cls.answersInt = tuple(str(i) for i in range(1, 7))
+        cls.answers_int = tuple(str(i) for i in range(1, 7))
 
-        cls.questionSingular = "???"
-        cls.answerSingular = ('ABC123!',)
+        cls.question_singular = "???"
+        cls.answer_singular = ("ABC123!",)
 
-    def testNoOptions(self):
+        cls.bool_question_str = "Does it taste like chicken?"
+
+    def test_no_options(self):
         """
-        Test whether multipleChoiceQuestion can correctly detect no options
+        Test whether multiple_choice_question can correctly detect no options
         """
 
         logging.info(
-            "Testing whether multipleChoiceQuestion can correctly detect no options")
+            "Testing whether multiple_choice_question can correctly detect no options"
+        )
 
-        testError = False
+        test_error = False
 
         try:
-            gloomuser.multipleChoiceQuestion("Well?", ())
+            self.user_interface.multiple_choice_question("Well?", ())
         except AssertionError:
-            testError = True
+            test_error = True
         finally:
-            self.assertTrue(testError)
+            self.assertTrue(test_error)
 
-    def testFaulOptions(self):
+    def test_faul_options(self):
         """
-        Test whether multipleChoiceQuestion can correctly detect double options
+        Test whether multiple_choice_question can correctly detect double options
         """
 
         logging.info(
-            "Testing whether multipleChoiceQuestion can correctly detect double options")
+            "Testing whether multiple_choice_question can correctly detect double options"
+        )
 
-        testError = False
+        test_error = False
 
         try:
-            gloomuser.multipleChoiceQuestion("Well?", self.faulOptions)
+            self.user_interface.multiple_choice_question(
+                "Well?",
+                self.faul_options
+            )
         except AssertionError:
-            testError = True
+            test_error = True
         finally:
-            self.assertTrue(testError)
+            self.assertTrue(test_error)
 
-    @patch('builtins.input', return_value='sour')
-    def testQuestionString(self, input):
+    @patch("builtins.input", return_value="sour")
+    def test_question_string(self, input):
         """
-        Test whether multipleChoiceQuestion can correctly parse str answers
-        """
-
-        logging.info(
-            "Testing whether multipleChoiceQuestion can correctly parse str answers")
-
-        self.assertEqual(gloomuser.multipleChoiceQuestion(
-            self.questionStr, self.answersStr), 'sour')
-
-    @patch('builtins.input', return_value='4')
-    def testQuestionInt(self, input):
-        """
-        Test whether multipleChoiceQuestion can correctly parse int answers
+        Test whether multiple_choice_question can correctly parse str answers
         """
 
         logging.info(
-            "Testing whether multipleChoiceQuestion can correctly parse int answers")
+            "Testing whether multiple_choice_question can correctly parse str answers"
+        )
 
-        self.assertEqual(gloomuser.multipleChoiceQuestion(
-            self.questionInt, self.answersInt), '4')
+        answer = self.user_interface.multiple_choice_question(
+            options=self.answers_str
+        )
 
-    @patch('builtins.input', return_value='2')
-    def testQuestionStringShortHand(self, input):
+        self.assertEqual(answer, "sour")
+
+    @patch("builtins.input", return_value="4")
+    def test_question_int(self, input):
         """
-        Test whether multipleChoiceQuestion can correctly parse int shorthand answers
-        """
-
-        logging.info(
-            "Testing whether multipleChoiceQuestion can correctly parse int shorthand answers")
-
-        self.assertEqual(gloomuser.multipleChoiceQuestion(
-            self.questionStr, self.answersStr), 'sour')
-
-    @patch('builtins.input', return_value='A')
-    def testQuestionSingular(self, input):
-        """
-        Test whether multipleChoiceQuestion can correctly parse short hand answers and single options
+        Test whether multiple_choice_question can correctly parse int answers
         """
 
         logging.info(
-            "Testing whether multipleChoiceQuestion can correctly parse short hand answers and single options")
+            "Testing whether multiple_choice_question can correctly parse int answers"
+        )
 
-        self.assertEqual(gloomuser.multipleChoiceQuestion(
-            self.questionSingular, self.answerSingular), 'ABC123!')
+        answer = self.user_interface.multiple_choice_question(
+            options=self.answers_int,
+            question=self.question_int,
+            range_options="blabla"
+        )
 
+        self.assertEqual(answer, "4")
 
-class TestYesNoQuestion(unittest.TestCase):
-    """
-    Test Gloomuser's yesNoQuestion function
-    """
-
-    @classmethod
-    def setUpClass(cls):
+    @patch("builtins.input", return_value="2")
+    def test_question_string_shortHand(self, input):
         """
-        Set up variables for testing
-        """
-
-        logging.info(
-            "Setting up variables for testing yesNoQuestion function")
-
-        cls.questionStr = "Does it taste like chicken?"
-
-    @patch('builtins.input', return_value='yes')
-    def testAnswerYes(self, input):
-        """
-        Test whether yesNoQuestion can correctly parse 'yes'
+        Test whether multiple_choice_question can correctly parse int shorthand answers
         """
 
         logging.info(
-            "Testing whether yesNoQuestion can correctly parse 'yes'")
+            "Testing whether multiple_choice_question can correctly parse int shorthand answers"
+        )
 
-        self.assertTrue(gloomuser.yesNoQuestion(self.questionStr))
+        answer = self.user_interface.multiple_choice_question(
+            options=self.answers_str,
+            question=self.question_str
+        )
 
-    @patch('builtins.input', return_value='1')
-    def testAnswer1(self, input):
+        self.assertEqual(answer, "sour")
+
+    @patch("builtins.input", return_value="A")
+    def test_question_singular(self, input):
         """
-        Test whether yesNoQuestion can correctly parse '1'
-        """
-
-        logging.info(
-            "Testing whether yesNoQuestion can correctly parse '1'")
-
-        self.assertTrue(gloomuser.yesNoQuestion(self.questionStr))
-
-    @patch('builtins.input', return_value='y')
-    def testAnswerSingularYes(self, input):
-        """
-        Test whether yesNoQuestion can correctly parse 'y'
+        Test whether multiple_choice_question can correctly parse short hand answers and single options
         """
 
         logging.info(
-            "Testing whether yesNoQuestion can correctly parse 'y'")
+            "Testing whether multiple_choice_question can correctly parse short hand answers and single options"
+        )
 
-        self.assertTrue(gloomuser.yesNoQuestion(self.questionStr))
+        answer = self.user_interface.multiple_choice_question(
+            options=self.answer_singular,
+            question=self.question_singular
+        )
 
-    @patch('builtins.input', return_value='no')
-    def testAnswerNo(self, input):
+        self.assertEqual(answer, "ABC123!")
+
+    @patch("builtins.input", return_value="yes")
+    def test_answer_yes(self, input):
         """
-        Test whether yesNoQuestion can correctly parse 'no'
-        """
-
-        logging.info(
-            "Testing whether yesNoQuestion can correctly parse 'no'")
-
-        self.assertFalse(gloomuser.yesNoQuestion(self.questionStr))
-
-    @patch('builtins.input', return_value='2')
-    def testAnswer2(self, input):
-        """
-        Test whether yesNoQuestion can correctly parse '2'
+        Test whether yes_no_question can correctly parse "yes"
         """
 
         logging.info(
-            "Testing whether yesNoQuestion can correctly parse '2'")
+            "Testing whether yes_no_question can correctly parse 'yes'"
+        )
 
-        self.assertFalse(gloomuser.yesNoQuestion(self.questionStr))
+        self.assertTrue(
+            self.user_interface.yes_no_question(
+                question=self.question_str
+            )
+        )
 
-    @patch('builtins.input', return_value='n')
-    def testAnswerSingularNo(self, input):
+    @patch("builtins.input", return_value="1")
+    def test_answer_1(self, input):
         """
-        Test whether yesNoQuestion can correctly parse 'n'
+        Test whether yes_no_question can correctly parse "1"
         """
 
         logging.info(
-            "Testing whether yesNoQuestion can correctly parse 'n'")
+            "Testing whether yes_no_question can correctly parse '1'"
+        )
 
-        self.assertFalse(gloomuser.yesNoQuestion(self.questionStr))
+        self.assertTrue(self.user_interface.yes_no_question())
+
+    @patch("builtins.input", return_value="y")
+    def test_answer_singular_yes(self, input):
+        """
+        Test whether yes_no_question can correctly parse "y"
+        """
+
+        logging.info(
+            "Testing whether yes_no_question can correctly parse 'y'"
+        )
+
+        self.assertTrue(
+            self.user_interface.yes_no_question(
+                question=self.question_str
+            )
+        )
+
+    @patch("builtins.input", return_value="no")
+    def test_answer_no(self, input):
+        """
+        Test whether yes_no_question can correctly parse "no"
+        """
+
+        logging.info(
+            "Testing whether yes_no_question can correctly parse 'no'"
+        )
+
+        self.assertFalse(
+            self.user_interface.yes_no_question(question=self.question_str)
+        )
+
+    @patch("builtins.input", return_value="2")
+    def test_answer_2(self, input):
+        """
+        Test whether yes_no_question can correctly parse "2"
+        """
+
+        logging.info(
+            "Testing whether yes_no_question can correctly parse '2'"
+        )
+
+        self.assertFalse(
+            self.user_interface.yes_no_question(question=self.question_str)
+        )
+
+    @patch("builtins.input", return_value="n")
+    def test_answer_singular_no(self, input):
+        """
+        Test whether yes_no_question can correctly parse "n"
+        """
+
+        logging.info(
+            "Testing whether yes_no_question can correctly parse 'n'"
+        )
+
+        self.assertFalse(
+            self.user_interface.yes_no_question()
+        )
 
 
 if __name__ == "__main__":
